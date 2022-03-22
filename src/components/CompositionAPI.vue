95afcaebdcb31dd21ffa7366ff46f8938532794b
wzy123321 <2662727613@qq.com>
2022-03-22 17:53:01 +0800

    3.22 17:52

--- src/components/CompositionAPI.vue
+++ src/components/CompositionAPI.vue
@@ -1,24 +1,40 @@
 <template>
   <div>
     <h2>Composition API</h2>
-    <h2>{{ state.count }}</h2>
-    <button @click="increment">+1</button>
+    <h2>reactive:{{ state.count }}</h2>
+    <h2>ref:{{num}}</h2>
+    <button @click="increment">reactive +1</button>
+    <button @click="ref_increment">ref +1</button>
   </div>
 </template>
 
 <script>
-  import { reactive } from 'vue'
+  import { reactive, ref ,computed,toRefs} from 'vue'
   export default {
     setup() {
+      // reactive的使用
       const state = reactive({
         count: 1
       })
+      
       const increment = () => {
         state.count++
       }
+
+      // ref的使用
+      const num = ref(0)
+      let num2= computed(()=>num.value*2)
+      const ref_increment = () => {
+        num.value++
+        console.log(num2.value)
+      }
+
+
       return {
         state,
-        increment
+        increment,
+        num,
+        ref_increment,
       }
     }
   }
